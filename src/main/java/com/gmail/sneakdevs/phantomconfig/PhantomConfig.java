5c5928d1e24e463d9f56f20ea72bc8e857e801ac
HaXr <haxr@outlook.dk>
2023-08-07 02:34:06 +0200

    Changed the name of the mod to include "Fork"
    Also added an icon

--- src/main/java/com/gmail/sneakdevs/phantomconfig/PhantomConfig.java
+++ src/main/java/com/gmail/sneakdevs/phantomconfig/PhantomConfig.java
@@ -1,5 +1,5 @@
 package com.gmail.sneakdevs.phantomconfig;
 
 public class PhantomConfig {
-    public static final String MODID = "phantomconfig";
+    public static final String MODID = "phantomconfigfork";
 }
